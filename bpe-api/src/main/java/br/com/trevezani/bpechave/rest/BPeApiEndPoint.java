c2b90a12c96f437d3f8ec1be574d1756dd3d1a56
Alberto Trevezani <trevezani@gmail.com>
2019-07-02 14:49:16 -0300

    Refactoring

--- bpe-api/src/main/java/br/com/trevezani/bpechave/rest/BPeApiEndPoint.java
+++ bpe-api/src/main/java/br/com/trevezani/bpechave/rest/BPeApiEndPoint.java
@@ -1,11 +1,10 @@
 package br.com.trevezani.bpechave.rest;
 
 import br.com.trevezani.bpechave.bean.ChaveBean;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
-import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -13,10 +12,7 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -29,11 +25,22 @@ public class BPeApiEndPoint {
     private ObjectMapper om = new ObjectMapper();
 
     @Inject
-    @ConfigProperty(name = "BPE_CHAVE_URL", defaultValue = "localhost")
+    @ConfigProperty(name = "url.bpechave", defaultValue = "localhost")
     private String urlBPeChave;
 
+    @GET
+    @Path("versao")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response doGetversao() {
+        JsonObjectBuilder json = Json.createObjectBuilder();
+        json.add("versao", "1.00");
+
+        return Response.ok(json.build()).build();
+    }
+
+
     @POST
-    @Path("/chave")
+    @Path("chave")
     @Produces({MediaType.APPLICATION_JSON})
     @Consumes(MediaType.APPLICATION_JSON)
     public Response doGetChaveBean(ChaveBean bean) {
@@ -41,47 +48,58 @@ public class BPeApiEndPoint {
             return Response.status(400,"Parâmetros inválidos").build();
         }
 
-        String chave = null;
+        String chave = "NA";
+        String error = null;
 
         try {
             chave = getChaveService(bean);
 
         } catch (Exception e) {
-            return Response.serverError().entity(e.toString()).build();
+            error = e.toString();
         }
 
         JsonObjectBuilder json = Json.createObjectBuilder();
         json.add("chbpe", chave);
 
+        if (chave.equals("NA")) {
+            json.add("url.bpechave", urlBPeChave);
+        }
+
+        if (error != null) {
+            json.add("error", error);
+        }
+
         return Response.ok(json.build()).build();
     }
 
-    @Timeout(500)
-    @Retry(maxRetries = 3)
-    @Fallback(fallbackMethod= "fallbackForChaveService")
-    private String getChaveService(ChaveBean bean) throws JsonProcessingException {
-        String beanJsonString = om.writeValueAsString(bean);
-
-        Client client = ClientBuilder.newClient();
-
-        final Response response = client.target("http://" + urlBPeChave + ":8082")
-                .path("chave")
-                .path("bean")
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .post(Entity.json(beanJsonString));
-
-        JsonObject obj = response.readEntity(JsonObject.class);
-
-        String chave = "NA";
-
-        if (obj != null) {
-            chave = obj.getString("chbpe");
-        }
-
-        return chave;
+    private String getChaveService(ChaveBean bean) throws Exception {
+        JsonObject obj = getChaveBean(om.writeValueAsString(bean));
+        return obj.getString("chbpe");
     }
 
-    private String fallbackForChaveService() {
-        return "NA";
+    @Timeout(200)
+    @CircuitBreaker
+    @Fallback(fallbackMethod = "getChaveBeanFallBack")
+    private JsonObject getChaveBean(final String beanJsonString) {
+        Client client = ClientBuilder.newClient();
+
+        try {
+            final Response response = client.target("http://" + urlBPeChave + ":8082")
+                    .path("chave")
+                    .path("bean")
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .post(Entity.json(beanJsonString));
+
+            return response.readEntity(JsonObject.class);
+        } catch (Exception e) {
+            return getChaveBeanFallBack(beanJsonString);
+        }
+    }
+
+    private JsonObject getChaveBeanFallBack(final String beanJsonString) {
+        JsonObjectBuilder json = Json.createObjectBuilder();
+        json.add("chbpe", "NA");
+
+        return json.build();
     }
 }
